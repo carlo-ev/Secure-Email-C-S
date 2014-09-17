4ea9317e9745d199ad40d4cd06feefba440c7ed3
carlo-ev <carlo-ev@unitec.edu>
2014-09-17 12:29:16 +0000

    almost done with selver side methods

--- applicationHerald.py
+++ applicationHerald.py
@@ -17,51 +17,100 @@ class ApplicationHerald:
 	def newRequest(self, message):
 		jsonMessage = json.loads(message)
 		print jsonMessage
+		user = self.get_user_from_message(jsonMesssage)
 		action = jsonMessage["action"]
-		if "user" not in jsonMessage:
-			user = ""
+		if( user is not None ):
+			if(islogged(user)):
+				if action == "getMail":
+					resp = self.getMail(user[0])
+				elif action == "getContacts":
+					resp = self.getContacts(user[0])
+				elif action == "sendMail":
+				elif action == "logout":
+				else:
+					print "Action not known"
+					resp = '{ success: false, errors: "Action not known" }'
+			else:
+				print "Invalid Credencials Sent"
+				resp = '{ success: false, errors: "Invalid Credentials"}'
 		else:
-			user = jsonMessage["user"]
-		response = ""
-		if( action == "login" ):
-			print "login"
-			email = str(jsonMessage["login"]["email"])
+			if action == "login":
+				print "login"
+				if self.login( str(jsonMessage["login"]["email"]), str(jsonMessage["login"]["password"]) ):
+					key = "RandomKey"
+					self.onlineUsers.append( (str(jsonMessage["login"]["email"]) , key) )
+					resp = self.login_response( (str(jsonMessage["login"]["email"]), key, "" )
+				else:
+					resp = self.login_response( "", "", "Invalid Credentials" )
+			elif action == "signup":
+				print "signup"
+				resp = "signup" 
+			else:
+				print "Action not known"
+				resp = '{ success: False, errors: "Action not known" }'
+		return resp
+
+	def getContacts(self, user_email):
+		print "Gettin user contacts"
+		current_user self.session.query(User).filter( User.email==user_email ).all()
+		contacts = current_user.contacts
+		print contacts
+		response = '{ successs: True, contacts: ['
+		for cont in contacts:
+			response += cont.toJSON()
+			response += ', '
+		response += '{} ] }'
+		return response
+																		 ' 
+																		 
+	def getMail(self, user_email):
+		print "Gettin Mail for ", user_email
+		messages = self.session.query(Message).filter( Message.receiver.like(user_email) ).all()
+		print messages
+		response = '{ succcess: True, messages: ['
+		for mess in messages:
+			response += mess.toJSON()
+			response += ', '
+		response += '{} ] }'
+		return response
+
+	def get_user_from_message(self, json_message):
+			user = None
+			if "user" not in json_message:
+				user = None
+			else:
+				json_user = json_message["user"]
+				if "email" and "key" not in json_user:
+					user = None
+				else:
+					user = (json_user["email"], json_user["key"])
+			return user
+	
+	def islogged(self, user):
+		search = [ i for i, v in enumerate(self.onlineUsers) if i == user[0] and v == user[1] ]
+		return len(search) > 0
+	
+	def login(self, email, password):
 			print email
-			password = str(jsonMessage["login"]["password"])
 			print password
-			user_to_login = self.session.query(User).filter(User.email.like(email)).all()
+			user_to_login = self.session.query(User).filter(User.email==email).all()
 			print user_to_login
-			if len(users) > 0:
+			if len(user_to_login) > 0:
 				print "User Found!"
 				if users[0].password == password:
-					key = "RandomKey"
-					self.onlineUsers.append((users[0].email, key))
-					print "Login Accepted!"
-					response = "{ user: {"
-					response +=  "email: " + users[0].email + ", key: " key
-					response += 
-					response = "{success: true}"
-				else:
-					print "Sorry Username/Password didnt match"
-					response = "{success: false, errors: 'username/password incorrect'}"	
-			else:
-				print "User not found!"
-				response = "{success: false, errors: 'username/password incorrect'}"	
-		elif( action == "getMail" ):
-			print "get mail"
-			if user != "":
-				
-				messages = self.session.query(Message).filter() 
-		elif( action == "getContacts" ):
-			#make get contacts
-			print "get Contacts"
-		elif( action == "sendMail" ):
-			#make send mail
-			print "send an email"
-		elif( action == "logout" ):
-			#make logout
-			print "logout user"
+					return True
+			return False
+
+	def login_response(self, user, key, errors):
+		response = '{'
+		if errors == "":
+				response += 'user: { '
+				response += 'email: "' + user + '",'
+				response += 'key: "' + key + '" '
+				response += '}, '
+				response += 'success: true'
 		else:
-			#make 
-			print "action not know"
+			response += 'errors: "' + errors + '", '
+			response += 'success: false'
+		response += '}'
 		return response
